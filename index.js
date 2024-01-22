8d16fd16df2ef630ce1f14feebe31df22ac178ea
Tobias Prima <primatjahja@gmail.com>
2024-01-22 22:35:51 +0800

    feat: query table based on selected user

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ let users = [
 ];
 
 async function checkVisisted() {
-  const result = await db.query("SELECT country_code FROM visited_countries");
+  const result = await db.query("SELECT country_code FROM visited_countries JOIN users ON users.id = user_id WHERE user_id = $1;", 
+  [currentUserId]);
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
